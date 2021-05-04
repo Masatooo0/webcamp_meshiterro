2d4aae93309f7ba96680d8510d0a951da04aefad
“Masatooo0” <masato.n.0321@gmail.com>
2021-05-04 02:17:38 +0000

    [add]refile,refile-mini_magick導入

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '35e031d215a9529134689483477ce80105f6ccb1dd273b66129181c7c48bded1a7605656dc5eab7ddbd43c41f24d71b9a0b8f4d63715d1f847825a18f870190c'
